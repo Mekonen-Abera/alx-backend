41a060fa199215856c1de247507dac4abd4da31f
Mekonen-Abera <makoninabera@gmail.com>
2024-09-02 06:26:15 +0000

    Pagination Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 """
-A function should return a tuple of size two containing 
-a start index and an end index
+Helper function for pagination
 """
 
 from typing import Tuple
-
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple of start and end indexes for a given pagination.
     """
-    return ((page_size * (page - 1)), page_size * page)
+    if page < 1 or page_size <= 0:
+        raise ValueError("Page must be >= 1 and page_size must be > 0")
+    
+    start_index = page_size * (page - 1)
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
