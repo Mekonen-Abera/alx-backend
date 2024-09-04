d6c4e60c92b0422c4389f206968677197f6a03bd
Mekonen-Abera <makoninabera@gmail.com>
2024-09-04 11:59:23 +0000

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3
-"""Defines a class BasicCache that inherits from
-BaseCaching and implements a basic caching system.
+"""Basic caching module.
 """
-
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache class for a simple caching system."""
-
-    def __init__(self):
-        """Initialize the cache."""
-        super().__init__()
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
+    """
     def put(self, key, item):
-        """Add an item to the cache.
-
-        Args:
-            key (str): The key under which the item is stored.
-            item (any): The item to store in the cache.
+        """Adds an item in the cache.
         """
-        if key is not None and item is not None:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieve an item from the cache.
-
-        Args:
-            key (str): The key of the item to retrieve.
-
-        Returns:
-            any: The item stored in the cache, or None if the key does not exist.
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
-
